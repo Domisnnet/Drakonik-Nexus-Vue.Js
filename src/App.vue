fcb66b38739dca29af9f6a44303dac9e6450215a
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2025-11-12 18:27:58 +0000

    OK

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <template>
-  <div
-    class="w-full h-screen bg-cover bg-center bg-no-repeat"
-    :style="{ backgroundImage: 'url(/images/fundo-space.jpg)' }"
-  >
-    <router-view />
-  </div>
+  <router-view />
 </template>
 
-<script setup lang="ts">
-// O App.vue é o ponto de entrada principal e apenas renderiza a rota atual.
-// O fundo é aplicado aqui para ser consistente em todas as páginas.
-</script>
+<script setup lang="ts"></script>
