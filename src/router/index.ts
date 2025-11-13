e94e641c41e65e18c745391edd47bf33282c588d
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2025-11-13 20:03:46 +0000

    ajust

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
-  { path: '/', name: 'home', component: () => import('@/views/HomeView.vue') },
-  { path: '/game', name: 'game', component: () => import('@/views/GameView.vue') },
+  { path: '/', name: 'home', component: () => import('../views/HomeView.vue') },
+  { path: '/game', name: 'game', component: () => import('../views/GameView.vue') },
 ]
 
 const router = createRouter({
